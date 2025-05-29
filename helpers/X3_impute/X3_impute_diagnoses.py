8a8c2f40adeb99191fcabcae89626097a8779ee4
Finn Fassbender <nnffssbndr@gmail.com>
2025-05-29 18:54:55 +0200

    prefer ICD-10 > ICD-9 in eICU, keep ICD version in final table

--- helpers/X3_impute/X3_impute_diagnoses.py
+++ helpers/X3_impute/X3_impute_diagnoses.py
@@ -29,8 +29,8 @@ class DiagnosesImputer(GlobalVars):
                 self.global_icu_stay_id_col,
             )
             .filter(
-                pl.col(self.global_hospital_stay_id_col).str.starts_with(
-                    "mimic"
+                pl.col(self.global_hospital_stay_id_col).str.contains_any(
+                    ["mimic", "nwicu"]
                 )
             )
             .group_by(self.global_hospital_stay_id_col)
@@ -54,24 +54,27 @@ class DiagnosesImputer(GlobalVars):
             pl.concat(
                 [
                     data.filter(
-                        pl.col(self.global_person_id_col).str.starts_with(
-                            "mimic"
+                        pl.col(self.global_person_id_col).str.contains_any(
+                            ["mimic", "nwicu"]
                         )
                     )
                     .drop(self.global_icu_stay_id_col)
                     .join(
                         IDs,
                         on=self.global_hospital_stay_id_col,
+                        how="left",
+                        coalesce=True,
                     )
-                    .explode(columns=[self.global_icu_stay_id_col]),
+                    .explode(self.global_icu_stay_id_col),
                     data.filter(
-                        ~pl.col(self.global_person_id_col).str.starts_with(
-                            "mimic"
+                        ~pl.col(self.global_person_id_col).str.contains_any(
+                            ["mimic", "nwicu"]
                         )
                     ),
                 ],
                 how="diagonal_relaxed",
             )
+            .collect()
             .with_columns(
                 # Impute missing ICD9 codes
                 pl.when(pl.col(self.diagnosis_icd_version_col) == 9)
@@ -93,7 +96,7 @@ class DiagnosesImputer(GlobalVars):
                 .replace("NoDx", None)
                 .alias(self.diagnosis_icd10_code_col),
             )
-            .select(
+            .group_by(
                 self.global_person_id_col,
                 self.global_hospital_stay_id_col,
                 self.global_icu_stay_id_col,
@@ -103,10 +106,69 @@ class DiagnosesImputer(GlobalVars):
                 self.diagnosis_end_col,
                 self.diagnosis_priority_col,
                 self.diagnosis_discharge_col,
-                self.diagnosis_description_col,
+                # self.diagnosis_description_col, # Description is not used in the grouping, as it can be inconsistent
+            )
+            # Add column for ICD version of original diagnosis code
+            .agg(
+                pl.implode(self.diagnosis_icd_version_col),
+                pl.first(self.diagnosis_description_col),
+            )
+            .with_columns(
+                pl.when(
+                    pl.col(self.diagnosis_icd_version_col)
+                    .list.unique()
+                    .list.len()
+                    > 1
+                )
+                .then(pl.lit("ICD-9 / ICD-10"))
+                .otherwise(
+                    pl.concat_str(
+                        pl.lit("ICD-"),
+                        pl.col(self.diagnosis_icd_version_col)
+                        .list.first()
+                        .cast(pl.String),
+                    )
+                )
+                .alias(self.diagnosis_icd_source_version_col)
             )
             .unique()
+            # Keep records that are not duplicates OR keep only active on discharge if duplicates exist
+            .with_columns(
+                # Count duplicates for each group
+                pl.len()
+                .over(
+                    self.global_person_id_col,
+                    self.global_hospital_stay_id_col,
+                    self.global_icu_stay_id_col,
+                    self.diagnosis_icd9_code_col,
+                    self.diagnosis_icd10_code_col,
+                    self.diagnosis_start_col,
+                )
+                .alias("Diagnosis Group Count")
+            )
+            .filter(
+                (pl.col("Diagnosis Group Count") == 1)
+                | (
+                    (pl.col("Diagnosis Group Count") > 1)
+                    & pl.col(self.diagnosis_discharge_col)
+                )
+            )
+            .drop("Diagnosis Group Count")
             .sort(self.global_icu_stay_id_col, self.diagnosis_start_col)
+            .select(
+                self.global_person_id_col,
+                self.global_hospital_stay_id_col,
+                self.global_icu_stay_id_col,
+                self.diagnosis_icd_source_version_col,
+                self.diagnosis_icd9_code_col,
+                self.diagnosis_icd10_code_col,
+                self.diagnosis_start_col,
+                self.diagnosis_end_col,
+                self.diagnosis_priority_col,
+                self.diagnosis_discharge_col,
+                self.diagnosis_description_col,
+            )
+            .lazy()
         )
 
 
