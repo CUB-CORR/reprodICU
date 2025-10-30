cd1c2b3e467733b151b2738bb50769ebea4f5d9a
Finn Fassbender <finn.fassbender@charite.de>
2025-10-30 12:34:12 +0100

    add mortality and x-free days

--- src/reprodICU/utils/__init__.py
+++ src/reprodICU/utils/__init__.py
@@ -1,3 +1,3 @@
-from . import scores, clinical, sepsis
+from . import scores, clinical, sepsis, mortality
 
-__all__ = ["scores", "clinical", "sepsis"]
+__all__ = ["scores", "clinical", "sepsis", "mortality"]
