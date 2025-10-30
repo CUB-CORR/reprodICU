44e8b3ffd965fa36db350441fe53cd2280820ab0
Finn Fassbender <finn.fassbender@charite.de>
2025-10-30 11:37:28 +0100

    add namespaces

---
+++ src/reprodICU/utils/scores/__init__.py
@@ -0,0 +1,13 @@
+from .SOFA import SOFA
+from .SOFA2 import SOFA2
+from .OASIS import OASIS, OASIS_icu_mortality, OASIS_hospital_mortality
+from .VIS import VIS
+
+__all__ = [
+    "SOFA",
+    "SOFA2",
+    "OASIS",
+    "OASIS_icu_mortality",
+    "OASIS_hospital_mortality",
+    "VIS",
+]
